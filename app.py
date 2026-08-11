aca40375fa1d04e28e147401688a8f00069cb2a6
Gautham J <gauthamjawahar91@gmail.com>
2026-08-11 20:03:04 +0530

    Course Updated in master

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ name = 'Gautham J learning git'
 
 print('welcome to the git learning',name)
 
-cource = 'git and github'
+cource = 'git and github learning totally waste of time'
 
 print('I am learning',cource)
 
