7d43a6127ed6c06291fb6bd66c853d7d8ece5b8f
Gautham J <gauthamjawahar91@gmail.com>
2026-08-12 17:23:38 +0530

    Github Connected Succesfully

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 print('Welcome to git_learning from chatgpt')
 
-
 name = 'Gautham J learning git'
-
 
 print('welcome to the git learning',name)
 
 cource = 'git and github learning must be very useful in future'
-
 
 print('I am learning',cource)
 
 profile = 'Full stack Developer'
 
 print(profile)
+
+print("Github Connection Successfully")
